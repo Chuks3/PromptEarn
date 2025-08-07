f92a2040392c49397b6110af43f9f3100d4361a0
Chuks <chuksonyekams@gmail.com>
2025-08-07 02:42:26 +0100

    Test

---
+++ src/App.vue
@@ -0,0 +1,540 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <main>
+    <Navbar />
+
+    <section class="sect1">
+      <div class="header-text">
+        <h2>Join over 135,641 people <span>earning</span> in dollars. Get started in 5 minutes</h2>
+
+        <p>We give you valuable digital and physcial products and make it easy for you to get paid promoting them from
+          anywhere in the world
+        </p>
+
+        <button>Get Started</button>
+      </div>
+
+      <div class="img-crop">
+        <img src="./assets/f40c032741f35b0beead69c72e87b7980c8e6cae.png" alt="A dashboard if the page">
+      </div>
+    </section>
+
+    <section class="sect2">
+      <div class="header-wrapper">
+        <h2>Why Choose Prompt Earn</h2>
+        <p>We give you valuable digital and physcial products and make it easy for you to get paid promoting them from
+          anywhere in the world</p>
+      </div>
+
+        <div class="img-wrapper">
+          <div class="img-cont">
+            <img src="./assets/receipt1.png" alt="A receipt">
+            <h3>Reliable Payment</h3>
+            <p>We have a solid payment system that ensures you get paid for every sale and receive your money in your preferred bank account whenever you request withdrawals on Sunday</p>
+          </div>
+          <div class="img-cont">
+            <img src="./assets/receipt2.png" alt="A receipt">
+            <h3>Commission</h3>
+            <p>At PromptEarn, you earn as high as 75% in z per sale. This means that for any of their products you sell, you earn up to half of the money (even more!)</p>
+          </div>
+          <div class="img-cont">
+            <img src="./assets/research-plan.png" alt="A receipt">
+            <h3>Product Marketplace</h3>
+            <p>We have a strong product vetting system. This ensures you’d only see high quality and fast selling products on our marketplace</p>
+          </div>
+          <div class="img-cont">
+            <img src="./assets/receipt3.png" alt="A receipt">
+            <h3>Reliable Payment</h3>
+            <p>To help improve your sales/marketing skills and make more money, you would have access to in-depth trainings.</p>
+          </div>
+        </div>
+    </section>
+
+    <section class="sect3">
+      <div class="sect-wrap">
+        <div class="header-wrapper">
+          <h2>Features of PromptEarn</h2>
+          <p>We give you valuable digital and physcial products and make it easy for you to get paid promoting them from
+            anywhere in the world</p>
+        </div>
+        
+        <div class="icons-cont">
+          <div class="icons">
+            <img class="icon1" src="./assets/icon1.png" alt="icon">
+            <h3>Landing Page Builder</h3>
+            <p>Create beautiful landing pages that drive sales and conversions.</p>
+          </div>
+          <div class="icons">
+            <img class="icon2" src="./assets/icon2.png" alt="icon">
+            <h3>Payment Links</h3>
+            <p>Our payment links are designed to simplify collections—fast, secure, and easy for anyone</p>
+          </div>
+          <div class="icons">
+            <img class="icon3" src="./assets/icon3.png" alt="icon">
+            <h3>Affiliates</h3>
+            <p>Earn effortlessly by sharing. Join our affiliate program and get rewarded for every referral.</p>
+          </div>
+          <div class="icons">
+            <img class="icon4" src="./assets/icon4.png" alt="icon">
+            <h3>Marketplace</h3>
+            <p>Discover, compare, and shop seamlessly. Our marketplace connects you with trusted vendors.</p>
+          </div>
+          <div class="icons">
+            <img class="icon5" src="./assets/icon5.png" alt="icon">
+            <h3>Seamless Payouts</h3>
+            <p>Enjoy fast, secure, and seamless payouts—designed to get your money where it belongs.</p>
+          </div>
+          <div class="icons">
+            <img class="icon6" src="./assets/icon6.png" alt="icon">
+            <h3>Courses and Learning Materials</h3>
+            <p>Access expert-led courses and curated learning materials to grow your skills anytime, anywhere.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="sect4">
+      <div class="sect-wrap">
+        <div class="header-wrapper">
+          <h2>Make more money by promoting good quality products from vetted vendors</h2>
+          <p>Register as a PromptEarn affiliate and gain instant access to our product marketplace. pick any of them that you like and start promoting it right away.</p>
+          <p>In addition, you’d also get access to regular training to strengthen your sales skills and earn more money weekly</p>
+          <button>Become and Affiliate</button>
+        </div>
+        
+        <div class="img">
+          <img src="./assets/sharpened-image.png" alt="">
+        </div>
+      </div>
+    </section>
+
+    <section class="sect5">
+      <div class="sect-wrap">
+        <div class="img">
+          <img src="./assets/image2.png" alt="">
+        </div>
+        
+        <div class="header-wrapper">
+          <h2>Let's help you sell more of your digital or physical products and reach a wider audience</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ipsam quis cum blanditiis molestias, rerum repellendus est perspiciatis provident quibusdam?</p>
+          <button>Become and Affiliate</button>
+        </div>  
+      </div>
+    </section>
+
+    <section class="sect6">
+      <div class="sect-wrapper">
+        <div class="text-wrapper">
+          <h2>How PromptEarn Works</h2>
+          <p>Register Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus praesentium rem modi aliquam tempora temporibus doloribus quidem dolore impedit ipsam!</p>
+        </div>
+
+          <div class="img">
+            <img src="./assets/video-image.jpg" alt="">
+          </div>
+      </div>
+    </section>
+
+    <section class="sect7">
+      <div class="sect-wrapper">
+        <div class="text-wrapper">
+          <h2>Yes, indeed, people sell and get paid every Sunday on PromptEarn</h2>
+          <p>Register Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus praesentium rem modi aliquam tempora temporibus doloribus quidem dolore impedit ipsam!</p>
+        </div>
+
+        <div class="img">
+          <img src="./assets/image3.png" alt="A series of cards">
+        </div>
+      </div>
+    </section>
+
+    <section class="sect8">
+      <div class="sect-wrapper">
+        <div class="text-wrapper">
+          <h2>Frequently Asked Questions</h2>
+            <p>Click here to get answers to your questions about PromptEarn</p>
+        </div>
+        
+        <div class="text-wrapper">
+          <div class="card">
+            <div class="card-wrapper">
+              <h3>What is PromptEarn About?</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, a maiores porro non quidem aliquid. Accusantium distinctio modi perspiciatis illo!</p>            
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-wrapper">
+              <h3>How do I become an affiliate on PromptEarn?</h3>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-wrapper">
+              <h3>How do I become a vendor?</h3>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-wrapper">
+              <h3>Do I earn in dollar on this platform?</h3>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-wrapper">
+              <h3>How do I make sales on PromptEarn?</h3>
+            </div>
+          </div>
+
+          <button>See all</button>
+        </div>
+      </div>
+    </section>
+
+    <section class="sect9">
+      <div class="sect-wrapper">
+        <div class="text-wrapper">
+          <h2>Ready to make more money promoting quality products to an international audience?</h2>
+          <p>Create your PromptEarn account in less than 5 minutes and start earning</p>
+          <button>Create Account</button>
+        </div>
+
+        <footer>
+          <div class="foot-text">
+            <img src="./assets/icon7.png" alt="icon">
+            <p>PromptEarn Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sed illum iste deserunt modi ad doloribus unde, voluptates nam pariatur iusto quas asperiores beatae rem?</p>
+
+            <div class="email">
+              <input type="email" name="" id="" placeholder="email address">
+              <button>Subscribe</button>
+            </div>
+          </div>
+          
+          <div class="foot-text">
+            <h3>Company</h3>
+            <ul>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+          
+          <div class="foot-text">
+            <h3>Legal</h3>
+            <ul>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+          
+          <div class="foot-text">
+            <h3>Social Links</h3>
+            <ul>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+        </footer>
+        <p class="copy">Copyright &copy; 2025 PromptEarn</p>
+      </div>
+    </section>
+
+  </main>
+</template>
+
+<style>
+main {
+  background-color: #fff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 97vw;
+  justify-self: center;
+  align-content: center;
+  text-align: center;
+}
+
+section {
+  max-width: 85%;
+  display: inherit;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  align-items: center;
+  align-content: center;
+  margin-top: 80px;
+}
+
+h2 {
+  font-size: 55px;
+}
+
+p {
+  font-size: 20px;
+  color: #292B2C;
+}
+button {
+  padding: 12px 21px;
+  border-radius: 15px;
+  font-size: 18px;
+  font-weight: 600;
+  border: none;
+  background-color: #B6FF34;
+  height: 50px;
+}
+img {
+  object-fit: fill;
+}
+
+.sect1 span {
+  color: #B6FF34;
+}
+.sect1 button, .sect8 button{
+  width: 210px;
+
+}
+.header-text {
+  width: 85%;
+}
+.header-text h2 {
+  margin-bottom: 0px;
+}
+ img {
+  max-width: 90%;
+}
+.img-crop {
+  width: 90%;
+  max-height: 30vh;
+  overflow: hidden;
+}
+.sect2 {
+  margin-top: 125px;
+}
+.sect2 .header-wrapper {
+  margin-top: 20px;
+  margin-bottom: 50px;
+  width: 90%;
+}
+.sect2 p, .sect2 h3 {
+  justify-self: center;
+  width: 80%;
+  text-align: center;
+}
+.sect2 img {
+  margin-top: 30px;
+  width: 280px;
+}
+
+.img-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 94%;
+}
+.img-cont {
+  overflow: hidden;
+  margin: 5px;
+  background-color: #EAECF0;
+  border-radius: 10px;
+  width: 45%;
+  min-width: 250px;
+  height: 450px;
+  margin-bottom: 25px;
+}
+
+.sect3 {
+  background-color: #E9FFEB;
+  min-width: 100%;
+}
+.sect-wrap {
+  margin-top: 100px;
+  margin-bottom: 150px;
+  max-width: 80%;
+}
+.sect3 .header-wrapper {
+  width: 70%;
+  justify-self: center;
+  margin-bottom: 100px;
+}
+.icons-cont {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.icons {
+  width: 30%;
+}
+.icons img {
+  border: 1px dotted black;
+  padding: 20px;
+  border-radius: 60%;
+}
+.icon1 {
+  background-color: #FCE2AF;
+}
+.icon2 {
+  background-color: #FFCABF;
+}
+.icon3 {
+  background-color: #E0D8FF;
+}
+.icon4 {
+  background-color: #C4EFFF;
+}
+.icon5 {
+  background-color: #B6FF34;
+}
+.icon6 {
+  background-color: #D6FFEB;
+}
+
+.sect4 {
+  background-color: #FFFFAA;
+  min-width: 100%;
+}
+.sect4 .sect-wrap, .sect5 .sect-wrap {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.sect4 .header-wrapper, .sect5 .header-wrapper {
+  width: 50%;
+  margin-top: 10px;
+  text-align: left;
+  min-width: 400px;
+}
+.sect4 h2, .sect4 p .sect5 h2, .sect5 p {
+  margin-bottom: 30px;
+}
+.sect4 .img, .sect5 .img {
+  width: 45%;
+  margin-top: 75px;
+}
+.sect5 {
+  min-width: 100%;
+  background-color: #AAFFB1;
+}
+.sect6 .sect-wrapper, .sect7 .sect-wrapper {
+  min-width: 80%;
+}
+.sect6 .text-wrapper, .sect7 .text-wrapper {
+  max-width: 87%;
+  justify-self: center;
+}
+.sect6 img, .sect7 img {
+  margin-top: 50px;
+  object-fit: cover;
+  max-height: 500px;
+  min-width: 100%;
+  border-radius: 20px;  
+}
+.sect8 {
+  min-width: 100%;
+  background-color: #FAFAFA;
+  padding-bottom: 200px;
+
+}
+.sect8 .sect-wrapper {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 50px;
+  text-align: left;
+}
+.sect8 .text-wrapper {
+  width: 50%;
+}
+
+.sect8 .card {
+  background-color: #fff;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  padding: 5px;
+  font-size: 10px;
+}
+.sect8 .card-wrapper{
+  margin-left: 10px;
+}
+.sect9 {
+  margin-top: 0px;
+  background-color: #FAFAFA;
+  min-width: 101%;
+}
+.sect9 .sect-wrapper {
+  min-width: 95%;
+  background-color: #012130;
+  color: #fff;
+  border-radius: 70px;
+  justify-items: center;
+}
+.sect9 p, .sect9 h2 {
+  color: #fff;
+  margin-bottom: 20px;
+}
+.sect9 .text-wrapper {
+  width: 70%;
+  margin-bottom: 50px;
+
+}
+.sect9 img {
+  width: 224px;
+  margin: 0px;
+}
+footer {
+  display: flex;
+  flex-wrap: wrap;
+  min-width: 60%;
+  justify-content: space-evenly;
+  text-align: left;
+  font-size: 16px;
+}
+.foot-text {
+  max-width: 25%;
+  justify-content: center;
+  
+}
+footer li, .foot-text p {
+  font-size: 16px;
+  list-style: none;
+  color: #999999;
+  margin-bottom: 15px;
+  text-align: left;
+}
+footer button {
+  background-color: #fff;
+}
+
+input {
+  padding: 15px 21px;
+  border-radius: 15px;
+  border: 1px solid #999;
+  background-color: #012130;
+  margin-right: 10px;
+}
+.copy {
+  display: block;
+  border-top: 1px solid #999;
+  color: #999;
+  background-color: #012130;
+  height: 30px;
+  overflow: hidden;
+  width: 101%;
+  padding-top: 10px;
+  
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <div class="nav-icon">
+      <img src="../assets/icon.png" alt="Navigation icon">
+    </div>
+
+    <div class="nav-links">
+      <a href="#">About Us</a>
+      <a href="#">Affiliates</a>
+      <a href="#">Vendors</a>
+      <a href="#">Blog</a>
+    </div>
+
+    <div class="nav-links">
+      <a href="#">Login</a>
+      <a href="#"><button>Create Account</button></a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 97vw;
+  height: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: center;
+  align-self: center;
+  position: fixed;
+  top: 0px;
+  background-color: #fff;
+}
+
+.nav-icon img {
+  height: 35px;
+  width: 155px;
+}
+
+.nav-links {
+  display: inherit;
+  gap: 15px;
+  justify-content: center;
+  align-items: center;
+}
+
+a {
+  font-size: 18px;
+  font-weight: 600;
+  text-decoration: none;
+  color: black;
+}
+
+button {
+  padding: 12px 21px;
+  border-radius: 15px;
+  font-size: 18px;
+  font-weight: 600;
+  border: none;
+  background-color: #B6FF34;
+}
+</style>
